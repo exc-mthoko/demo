5270b85ac5abd7afa3e400c628da6be3eaa875f3
exc-mthoko <hamiltonict@outlook.com>
2019-01-23 15:08:01 +0200

    Add files via upload

---
+++ agxgame.js
@@ -0,0 +1,269 @@
+var io;
+var gameSocket;
+
+/**
+ * This function is called by index.js to initialize a new game instance.
+ *
+ * @param sio The Socket.IO library
+ * @param socket The socket object for the connected client.
+ */
+exports.initGame = function(sio, socket){
+    io = sio;
+    gameSocket = socket;
+    gameSocket.emit('connected', { message: "You are connected!" });
+
+    // Host Events
+    gameSocket.on('hostCreateNewGame', hostCreateNewGame);
+    gameSocket.on('hostRoomFull', hostPrepareGame);
+    gameSocket.on('hostCountdownFinished', hostStartGame);
+    gameSocket.on('hostNextRound', hostNextRound);
+
+    // Player Events
+    gameSocket.on('playerJoinGame', playerJoinGame);
+    gameSocket.on('playerAnswer', playerAnswer);
+    gameSocket.on('playerRestart', playerRestart);
+}
+
+/* *******************************
+   *                             *
+   *       HOST FUNCTIONS        *
+   *                             *
+   ******************************* */
+
+/**
+ * The 'START' button was clicked and 'hostCreateNewGame' event occurred.
+ */
+function hostCreateNewGame() {
+    // Create a unique Socket.IO Room
+    var thisGameId = ( Math.random() * 100000 ) | 0;
+
+    // Return the Room ID (gameId) and the socket ID (mySocketId) to the browser client
+    this.emit('newGameCreated', {gameId: thisGameId, mySocketId: this.id});
+
+    // Join the Room and wait for the players
+    this.join(thisGameId.toString());
+};
+
+/*
+ * Two players have joined. Alert the host!
+ * @param gameId The game ID / room ID
+ */
+function hostPrepareGame(gameId) {
+    var sock = this;
+    var data = {
+        mySocketId : sock.id,
+        gameId : gameId
+    };
+    //console.log("All Players Present. Preparing game...");
+    io.sockets.in(data.gameId).emit('beginNewGame', data);
+}
+
+/*
+ * The Countdown has finished, and the game begins!
+ * @param gameId The game ID / room ID
+ */
+function hostStartGame(gameId) {
+    console.log('Game Started.');
+    sendWord(0,gameId);
+};
+
+/**
+ * A player answered correctly. Time for the next word.
+ * @param data Sent from the client. Contains the current round and gameId (room)
+ */
+function hostNextRound(data) {
+    if(data.round < wordPool.length ){
+        // Send a new set of words back to the host and players.
+        sendWord(data.round, data.gameId);
+    } else {
+        // If the current round exceeds the number of words, send the 'gameOver' event.
+        io.sockets.in(data.gameId).emit('gameOver',data);
+    }
+}
+/* *****************************
+   *                           *
+   *     PLAYER FUNCTIONS      *
+   *                           *
+   ***************************** */
+
+/**
+ * A player clicked the 'START GAME' button.
+ * Attempt to connect them to the room that matches
+ * the gameId entered by the player.
+ * @param data Contains data entered via player's input - playerName and gameId.
+ */
+function playerJoinGame(data) {
+    //console.log('Player ' + data.playerName + 'attempting to join game: ' + data.gameId );
+
+    // A reference to the player's Socket.IO socket object
+    var sock = this;
+
+    // Look up the room ID in the Socket.IO manager object.
+    var room = gameSocket.manager.rooms["/" + data.gameId];
+
+    // If the room exists...
+    if( room != undefined ){
+        // attach the socket id to the data object.
+        data.mySocketId = sock.id;
+
+        // Join the room
+        sock.join(data.gameId);
+
+        //console.log('Player ' + data.playerName + ' joining game: ' + data.gameId );
+
+        // Emit an event notifying the clients that the player has joined the room.
+        io.sockets.in(data.gameId).emit('playerJoinedRoom', data);
+
+    } else {
+        // Otherwise, send an error message back to the player.
+        this.emit('error',{message: "This room does not exist."} );
+    }
+}
+
+/**
+ * A player has tapped a word in the word list.
+ * @param data gameId
+ */
+function playerAnswer(data) {
+    // console.log('Player ID: ' + data.playerId + ' answered a question with: ' + data.answer);
+
+    // The player's answer is attached to the data object.  \
+    // Emit an event with the answer so it can be checked by the 'Host'
+    io.sockets.in(data.gameId).emit('hostCheckAnswer', data);
+}
+
+/**
+ * The game is over, and a player has clicked a button to restart the game.
+ * @param data
+ */
+function playerRestart(data) {
+    // console.log('Player: ' + data.playerName + ' ready for new game.');
+
+    // Emit the player's data back to the clients in the game room.
+    data.playerId = this.id;
+    io.sockets.in(data.gameId).emit('playerJoinedRoom',data);
+}
+
+/* *************************
+   *                       *
+   *      GAME LOGIC       *
+   *                       *
+   ************************* */
+
+/**
+ * Get a word for the host, and a list of words for the player.
+ *
+ * @param wordPoolIndex
+ * @param gameId The room identifier
+ */
+function sendWord(wordPoolIndex, gameId) {
+    var data = getWordData(wordPoolIndex);
+    io.sockets.in(data.gameId).emit('newWordData', data);
+}
+
+/**
+ * This function does all the work of getting a new words from the pile
+ * and organizing the data to be sent back to the clients.
+ *
+ * @param i The index of the wordPool.
+ * @returns {{round: *, word: *, answer: *, list: Array}}
+ */
+function getWordData(i){
+    // Randomize the order of the available words.
+    // The first element in the randomized array will be displayed on the host screen.
+    // The second element will be hidden in a list of decoys as the correct answer
+    var words = shuffle(wordPool[i].words);
+
+    // Randomize the order of the decoy words and choose the first 5
+    var decoys = shuffle(wordPool[i].decoys).slice(0,5);
+
+    // Pick a random spot in the decoy list to put the correct answer
+    var rnd = Math.floor(Math.random() * 5);
+    decoys.splice(rnd, 0, words[1]);
+
+    // Package the words into a single object.
+    var wordData = {
+        round: i,
+        word : words[0],   // Displayed Word
+        answer : words[1], // Correct Answer
+        list : decoys      // Word list for player (decoys and answer)
+    };
+
+    return wordData;
+}
+
+/*
+ *I Don't want to shuffle
+ */
+function shuffle(array) {
+
+    /*var currentIndex = array.length;
+    var temporaryValue;
+    var randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }*/
+    return array;
+}
+
+/**
+ * Each element in the array provides data for a single round in the game.
+ *
+ * In each round, two random "words" are chosen as the host word and the correct answer.
+ * Five random "decoys" are chosen to make up the list displayed to the player.
+ * The correct answer is randomly inserted into the list of chosen decoys.
+ *
+ * @type {Array}
+ */
+var wordPool = [
+    {
+        "words"  : [ "The Godfather","1972" ],
+        "decoys" : [ "2015","2014","2000","1995","1979","2013","2001","2002","2007","1989" ]
+    },
+    {
+        "words"  : [ "The Dark Knight","2008" ],
+        "decoys" : [ "2006","2004","1992","2015","2000","1977","1980","1990","1988","1960" ]
+    },
+	
+	{
+        "words"  : [ "The Matrix","1999" ],
+        "decoys" : [ "1997","1899","1975","2000","2005","2015","1014","2001","1984","1965" ]
+    },
+
+    {
+        "words"  : [ " Schindler's List","1993" ],
+        "decoys" : [ "2000","1992","1990","2004","2015","2013","1999","1982","1970","1965" ]
+    },
+	
+	{
+        "words"  : [ "12 Angry Men","1957" ],
+        "decoys" : [ "2005","2015","2014","2008","2013","1988","1960","1970","1985","1895" ]
+    },
+
+    {
+        "words"  : [ "Inception","2010" ],
+        "decoys" : [ "1960","1930","1954","1854","1799","1997","1990","2009","2011","1600" ]
+    }
+	,
+
+    {
+        "words"  : [ "The Lion King","1994" ],
+        "decoys" : [ "2000","2001","2005","1999","1973","1990","2014","2015","1997","1980" ]
+    }
+	,
+
+    {
+        "words"  : [ "Pulp Fiction","1994" ],
+        "decoys" : [ "2000","1950","1920","1935","1895","1800","1981","1867","1945","1965" ]
+    }
+]
